3bffd9d5efa1e749028056a2109d1eb088d0c32a
Monique <skaiwilliams85@gmail.com>
2025-12-19 11:51:58 -0500

    updated handleFolloweUser in Profile Summary to update state locally (optimistically)

--- src/components/profile/ProfileSummary.tsx
+++ src/components/profile/ProfileSummary.tsx
@@ -6,13 +6,20 @@ import { useModal } from "@/context/useModal";
 import useFollowStates from "@/hooks/useFollowStates";
 import { ModalEnum } from "@/types/modal";
 import { Profile } from "@/types/user";
-import { useQueryClient } from "@tanstack/react-query";
 import { toggleFollowUser } from "@utils/api/user/follow-user";
+import { useEffect, useState } from "react";
 
 type Props = {
   profile: Profile;
   userID: Profile["user_id"] | null;
 };
+
+enum Counts {
+  F = "FOLLOWERS",
+  FL = "FOLLOWING",
+}
+
+type State = Counts;
 
 function ProfileSummary({ profile, userID }: Props) {
   const { dispatch } = useModal();
@@ -26,50 +33,80 @@ function ProfileSummary({ profile, userID }: Props) {
     bio,
   } = profile;
 
-  const queryClient = useQueryClient();
   const { data, refetch } = useFollowStates(userID, profileID);
-  const { followers, following, isFollowing } = data ?? {
-    followers: 0,
-    following: 0,
-    isFollowing: false,
-  };
+
+  // Use local state since we want to manipulate the state to acheive 'optimistic' state changes
+  const [followCounts, setFollowCounts] = useState<typeof data>(data);
+
+  console.log(followCounts);
 
   const isLoggedIn = Boolean(userID);
 
   // Is the current user viewing their own profile?
   const isUserViewingSelf = profileID === userID;
 
-  /**
-   * Handles following or unfollowing a user.
-   *
-   * Uses the `userID` to determine is a current user is present. (Is there a session present? Is the user logged in?)
-   *
-   * Follow the target user if logged in, otherwise prompt the user to log in.
-   */
-  async function handleFollowUser() {
-    console.log("handling follow user");
-    // If no user, prompt user to log in
-    if (!userID) {
-      return dispatch({
-        type: ModalActionEnum.OPEN_MODAL,
-        payload: {
-          type: ModalEnum.A,
-        },
-      });
-    }
+  // Sync local state whenever the query data changes
+  useEffect(() => {
+    if (data) setFollowCounts(data);
+  }, [data]);
 
-    // Toggle follow user & re-fetch follow counts
-    const { error } = await toggleFollowUser(profile.user_id);
-    if (!error) refetch();
-
-    // Invalidate the query that gets the follower information
-    queryClient.invalidateQueries({
-      queryKey: ["get-followers", profile.user_id],
+  function openLoginModal() {
+    dispatch({
+      type: ModalActionEnum.OPEN_MODAL,
+      payload: { type: ModalEnum.A },
     });
   }
 
+  function updateFollowCounts(state: State, increment = false) {
+    if (!followCounts) return;
+
+    const obj = Object.assign({}, followCounts);
+    const count = increment ? 1 : -1;
+
+    const modifiedObj = (() => {
+      switch (state) {
+        case Counts.F:
+          return Object.assign(obj, {
+            followers: obj.followers + count,
+            isFollowing: !obj.isFollowing,
+          });
+
+        case Counts.FL:
+          return obj;
+
+        default:
+          return obj;
+      }
+    })();
+
+    setFollowCounts(modifiedObj);
+  }
+
+  /**
+   * Handles following or unfollowing a user.
+   * If `!loggedIn` open the login modal.
+   * Otherwise, immediately update the follow count, then the target user.
+   * Refetch the data after following/unfollowing the user.
+   */
+  async function handleFollowUser() {
+    if (!isLoggedIn) return openLoginModal();
+
+    // Optimistically update follower count through local state
+    // Following the user? Decrement. Otherwise increment.
+    updateFollowCounts(Counts.F, !followCounts?.isFollowing);
+
+    // Toggle follow user & re-fetch follow counts
+    const { error } = await toggleFollowUser(profile.user_id);
+
+    if (error) {
+      return updateFollowCounts(Counts.F);
+    }
+
+    refetch();
+  }
+
   function handleViewFollowers() {
-    if (!followers) return;
+    if (!followCounts?.followers) return;
 
     dispatch({
       type: ModalActionEnum.OPEN_MODAL,
@@ -85,56 +122,63 @@ function ProfileSummary({ profile, userID }: Props) {
   return (
     <section className="py-4 max-w-[450px]">
       <article className="flex flex-col gap-1">
+        {/* Avatar */}
         <Avatar
           avatar={avatar}
           username={username}
           className={"size-10 rounded-full text-xl"}
         />
 
+        {/* Name */}
         <div className="mt-2">
           <h2 className="font-semibold text-lg capitalize text-neutral-800">
-            {firstname && (
-              <span>
-                {firstname} {lastname}
-              </span>
-            )}
-            {!firstname && username && <span>{username}</span>}
+            {!firstname && <span>{username}</span>}
+            <span>{firstname + " " + lastname}</span>
           </h2>
         </div>
 
-        {bio && <p className="text-zinc-500 leading-5 text-[13px]">{bio}</p>}
+        {/* Bio */}
+        <p className="text-zinc-500 leading-5 text-[13px]">{bio}</p>
 
+        {/* Follower counts & Following Actions */}
         <div className="flex flex-wrap gap-2 mt-1 font-semibold text-neutral-800 text-xs">
+          {/* Username */}
           <p className="">@{username}</p>
-
           <span>&#xb7;</span>
 
+          {/* Followers Count */}
           <button
             onClick={handleViewFollowers}
             className="font-semibold flex gap-1"
           >
-            <span>{followers}</span>
-            <span>{followers === 1 ? "Follower" : "Followers"}</span>
+            <span>{followCounts?.followers ?? 0}</span>
+            <span>
+              {followCounts?.followers === 1 ? "Follower" : "Followers"}
+            </span>
           </button>
 
           <span>&#xb7;</span>
 
-          <button>{following}&nbsp;Following</button>
+          {/* Following Count */}
+          <button onClick={handleViewFollowing}>
+            {followCounts?.following ?? 0}&nbsp;Following
+          </button>
 
+          {/* Follow Button Status */}
           {!isUserViewingSelf && (
             <>
               <span>&#xb7;</span>
-
               <button onClick={handleFollowUser} className="cursor-pointer">
-                {isFollowing && userID ? "Following" : "Follow"}
+                {followCounts?.isFollowing && isLoggedIn
+                  ? "Following"
+                  : "Follow"}
               </button>
             </>
           )}
 
-          {isUserViewingSelf && (
+          {isUserViewingSelf && isLoggedIn && (
             <>
               <span>&#xb7;</span>
-
               <button className="cursor-pointer">Edit Profile</button>
             </>
           )}
